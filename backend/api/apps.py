4d6cf491e41e3ecfa6dbfaca9ac9c5e2e9da2fac
Leonidkorotchenko <speedleo912@gmail.com>
2025-01-06 16:23:38 +0300

    add setup file

--- backend/api/apps.py
+++ backend/api/apps.py
@@ -1,6 +1,9 @@
+"""apps."""
 from django.apps import AppConfig
 
 
 class ApiConfig(AppConfig):
+    """class."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,14 @@
+"""serializers."""
 from rest_framework import serializers
 
 from .models import Task
 
 
 class TaskSerializer(serializers.ModelSerializer):
+    """class."""
+
     class Meta:
+        """Meta_class."""
+
         model = Task
         fields = ('id', 'title', 'description', 'completed')
